a096ef43602568250620de276905e19893e8eecf
jonathanli2 <jonathan_li2000@yahoo.ca>
2018-05-13 16:19:36 -0400

    support left right

--- LineFeed Keyboard/KeyboardViewController.swift
+++ LineFeed Keyboard/KeyboardViewController.swift
@@ -10,46 +10,34 @@ import UIKit
 
 class KeyboardViewController: UIInputViewController {
 
-    @IBOutlet var nextKeyboardButton: UIButton!
-    @IBOutlet var lineFeedButton: UIButton!
-    
-    override func updateViewConstraints() {
-        super.updateViewConstraints()
+  //  @IBOutlet var nextKeyboardButton: UIButton!
+ //   @IBOutlet var lineFeedButton: UIButton!
+  
+    @IBAction func onDirectionLeft(_ sender: Any) {
         
-        // Add custom view sizing constraints here
+        textDocumentProxy.adjustTextPosition(byCharacterOffset: -1)
+    }
+    @IBAction func onDirectionRight(_ sender: Any) {
+        
+        textDocumentProxy.adjustTextPosition(byCharacterOffset: 1)
+    }
+    @IBAction func onNextKeyboard(_ sender: Any) {
+        self.advanceToNextInputMode()
+    }
+    
+    @IBAction func onReturnPressed(_ sender: Any) {
+        (textDocumentProxy as UIKeyInput).insertText("\r")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: UIButtonType.custom)
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Show next keyboard"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .touchUpInside)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant:10).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -10).isActive = true
-        
-        // Perform custom UI setup here
-        self.lineFeedButton = UIButton(type: UIButtonType.custom)
-             self.lineFeedButton.setTitleColor(UIColor.purple, for: UIControlState.normal)
-        self.lineFeedButton.setTitle(NSLocalizedString("New Line", comment: "Add a new line"), for: [])
-        self.lineFeedButton.sizeToFit()
-        self.lineFeedButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.lineFeedButton.addTarget(self, action: #selector(keyPressed(sender:)), for: .touchUpInside)
-        
-        self.view.addSubview(self.lineFeedButton)
-        
-        self.lineFeedButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10).isActive = true
-        self.lineFeedButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -10).isActive = true
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+/*
     @objc func keyPressed(sender: AnyObject?) {
         let button = sender as! UIButton
         if (button == self.lineFeedButton){
@@ -57,10 +45,7 @@ class KeyboardViewController: UIInputViewController {
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated
-    }
+
     
     override func textWillChange(_ textInput: UITextInput?) {
         // The app is about to change the document's contents. Perform any preparation here.
@@ -78,5 +63,5 @@ class KeyboardViewController: UIInputViewController {
         }
         self.nextKeyboardButton.setTitleColor(textColor, for: [])
     }
-
+*/
 }
